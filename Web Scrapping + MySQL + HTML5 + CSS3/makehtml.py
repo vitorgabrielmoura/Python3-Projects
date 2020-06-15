edbaf800a06af712a64961a2259f2bfdea61eb70
Vitor Gabriel Moura de Melo <65458418+vitorgabrielmoura@users.noreply.github.com>
2020-06-15 10:41:40 -0300

    v1.5
    done some changes in the html and mainly css styles for the site

--- Web Scrapping + MySQL + HTML5 + CSS3/makehtml.py
+++ Web Scrapping + MySQL + HTML5 + CSS3/makehtml.py
@@ -22,18 +22,32 @@ with open('index.html', 'w+') as d:
     </head>
     <body>
     <header>
+    <hgroup>
     <img id="amazonlogo" src="site-resources/amazon.png" width = "150" height="50"">
     <h1 id="title"">WITHLIST </h1>
+    </hgroup>
     </header>
-    <div id="interface">""")
+    <div id="interface">
+    <h1 id="xesque"><a href="https://www.amazon.com.br/hz/wishlist/ls/1QPZQPIXMAFL?ref_=wl_share " target="_blank">My Amazon Withlist</a></h1>
+    <h4>Last time updated by Vitu was in 15/06/2020</h4>
+    <ul id="books" style="list-style-type:none">""")
     for x in result:
         d.write(f"""
-        <h1>{x['nome']}</h1>
-        <h2 style="">{x['autor']}</h2>
-        <h3>{x['preco']}</h3>
+        <li id="img"><img src="images/{x['nome'][:10]}.jpg"></img></li>
+        <li id="data"><ul>
+        <li><h1>{x['nome']}</h1></li>
+        <li><h2>{x['autor']}</h2></li>
+        <li><h3>{x['preco']}</h3></li>
+        </ul>
+        </li>
+        <div id= "line"></div>
         """)
     d.write("""
+    </ul>
     </div>
+    <footer id="rodape">
+    <div id="rodape" style="text-align: center;">Copyright 2020 &copy;&trade; - by Vitu Gabriel</div>
+    </footer>
     </body>
     </html>
     """)
